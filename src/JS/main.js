b7b4afb09f8d379e498c3963deefbfb1577621d5
RicaldoniDev <102675359+RicaldoniDev@users.noreply.github.com>
2022-10-29 18:08:54 -0300

    correções finais

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,3 +1,7 @@
 document.getElementById("sign-in").onclick = function () {
     location.href = "src/HTML/sign-in.html";
 };
+
+document.getElementById("sign-up").onclick = function () {
+    location.href = "src/HTML/sign-up.html";
+};
