0285291ebce0e0421903f8451c52c2244f647747
RicadoniDev <julianoricaldoni@gmail.com>
2022-11-07 15:27:54 -0300

    adicição dos favoritos

---
+++ src/JS/avaliacao.js
@@ -0,0 +1,6 @@
+document.getElementById("mostrar mais").onmouseover = function() {
+	document.getElementById("mais").style.display = "block";
+	document.getElementById("ponto").style.display = "none";
+	document.getElementById("mostrar mais").style.display = "none";
+	console.log("Lixo");
+}
