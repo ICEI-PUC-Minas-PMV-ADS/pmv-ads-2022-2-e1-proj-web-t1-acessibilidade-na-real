789c6d695c25462a6ee9d999a7a08ddbac7124c7
RicadoniDev <julianoricaldoni@gmail.com>
2022-11-08 18:02:16 -0300

    Atualização da página de avaliações

--- src/JS/avaliacao.js
+++ src/JS/avaliacao.js
@@ -1,6 +1,19 @@
-document.getElementById("mostrar mais").onmouseover = function() {
-	document.getElementById("mais").style.display = "block";
-	document.getElementById("ponto").style.display = "none";
-	document.getElementById("mostrar mais").style.display = "none";
-	console.log("Lixo");
+document.getElementById("mostrar-mais").onclick = function(){ 
+	// Cria as variáveis que irão armazenar os elementos
+	var pontos = document.getElementById("ponto");
+	var maisTexto = document.getElementById("mais");
+	var textoBtn = document.getElementById("mostrar-mais");
+
+	// Verifica se o texto está expandido ou não
+	if (pontos.style.display === "none") {
+		// altera os elementos para o estado inicial
+		pontos.style.display = "inline";
+		textoBtn.innerHTML = "Mais";
+		maisTexto.style.display = "none";
+	} else {
+		// altera os elementos para o estado expandido
+		pontos.style.display = "none";
+		textoBtn.innerHTML = "Menos";
+		maisTexto.style.display = "inline";
+	}
 }
