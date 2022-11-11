6e884865eae5dd2491ab0d85586adbfa9d7c40ed
leandrobisaias <lbisaias@sga.pucminas.br>
2022-11-10 21:11:49 -0300

    inserção da página de estabelecimento

---
+++ src/CSS/stylesheet.css
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<div class="estrelas">
+  <input type="radio" id="cm_star-empty" name="fb" value="" checked/>
+  <label for="cm_star-1"><i class="fa"></i></label>
+  <input type="radio" id="cm_star-1" name="fb" value="1"/>
+  <label for="cm_star-2"><i class="fa"></i></label>
+  <input type="radio" id="cm_star-2" name="fb" value="2"/>
+  <label for="cm_star-3"><i class="fa"></i></label>
+  <input type="radio" id="cm_star-3" name="fb" value="3"/>
+  <label for="cm_star-4"><i class="fa"></i></label>
+  <input type="radio" id="cm_star-4" name="fb" value="4"/>
+  <label for="cm_star-5"><i class="fa"></i></label>
+  <input type="radio" id="cm_star-5" name="fb" value="5"/>
+</div>
