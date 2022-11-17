e6061b944a93cffc41f496ab8d92632d469a2e30
RicaldoniDev <102675359+RicaldoniDev@users.noreply.github.com>
2022-11-16 22:55:41 -0300

    Descobri localstorage, descobri que não serve na pag

--- src/JS/avaliacao.js
+++ src/JS/avaliacao.js
@@ -1,6 +1,5 @@
 function classificacaoNota(item){
     var nota = item.id;
-    sessionStorage.valor = nota;
     for (var i = 1; i <= 5; i++) {
         if (i <= nota) {
             document.getElementById(i).style.color = "orange";
@@ -12,4 +11,18 @@ function classificacaoNota(item){
     document.getElementById("nota").value = parseInt(nota);
 }
 
-function enviar(){}
+function send(){
+    var nota = document.getElementById("nota").value;
+    var comentario = document.getElementById("comentario").value;
+    if (nota != 0) {
+        var dados = {"nota": nota, "comentario": comentario};
+        localStorage.setItem("dados", JSON.stringify(dados));
+        alert("Avaliação enviada com sucesso!");
+        window.location.href = "../../index.html";
+        return 0;
+    }
+    else {
+        alert("Selecione uma nota");
+        return 0;
+    }
+}
