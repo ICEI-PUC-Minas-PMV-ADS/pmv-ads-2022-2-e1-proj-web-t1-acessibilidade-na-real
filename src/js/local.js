03b2ddcbd00ce654eec3a48b58adcfd6c97044b0
RicaldoniDev <102675359+RicaldoniDev@users.noreply.github.com>
2022-11-26 15:57:25 -0300

    funcionalidade cadastro local

--- src/js/local.js
+++ src/js/local.js
@@ -5,22 +5,37 @@ function valida_form (){
 	var numero = document.getElementById("numero").value;
 	var bairro = document.getElementById("bairro").value;
 	var estado = document.getElementById("estado").value;
-	var acessilibilidade = document.getElementsByTagName("select")[2];
-	console.log(acessilibilidade);
-	// function getSelectValues(select) {
-	// 	var result = [];
-	// 	var options = select && select.options;
-	// 	var opt;
-	  
-	// 	for (var i=0, iLen=options.length; i<iLen; i++) {
-	// 	  opt = options[i];
-	  
-	// 	  if (opt.selected) {
-	// 		result.push(opt.value || opt.text);
-	// 	  }
-	// 	}
-	// 	return result;
-	//   }
+	var acessilibilidade = getSelectValues(document.getElementsByTagName("select")[2]);
+	
+	if (nomeLocal == "" || categoria == "" || rua == "" || numero == "" || bairro == "" || estado == "" || acessilibilidade == ""){
+		alert("Preencha todos os campos!");
+		return false;
+	} else {
+		localStorage.setItem("nomeLocal", nomeLocal);
+		localStorage.setItem("categoria", categoria);
+		localStorage.setItem("rua", rua);
+		localStorage.setItem("numero", numero);
+		localStorage.setItem("bairro", bairro);
+		localStorage.setItem("estado", estado);
+		localStorage.setItem("acessilibilidade", acessilibilidade);
+		alert("Local cadastrado com sucesso!");
+		window.location.href = "home.html";
+	}
+	
+	function getSelectValues(select) {
+	 	var result = [];
+	 	var options = select && select.options;
+	 	var opt;
+	
+	 	for (var i=0, iLen=options.length; i<iLen; i++) {
+	 	  opt = options[i];
+	
+	 	  if (opt.selected) {
+	 		result.push(opt.value || opt.text);
+	 	  }
+	 	}
+	 	return result;
+	}
 }
 
 
